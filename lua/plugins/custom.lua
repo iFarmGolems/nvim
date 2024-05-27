aeb763832888bcda37e325493434c9d5bea63330
Patrik Jajcay <patrik.jajcay@microstep-mis.com>
2024-05-27 16:01:42 +0200

    Base config

---
+++ lua/plugins/custom.lua
@@ -0,0 +1,19 @@
+return {
+  -- add gruvbox
+  { "ellisonleao/gruvbox.nvim" },
+
+  -- Configure LazyVim to load gruvbox
+  {
+    "LazyVim/LazyVim",
+    opts = {
+      colorscheme = "gruvbox",
+    },
+  },
+
+  -- change trouble config
+  {
+    "folke/trouble.nvim",
+    -- opts will be merged with the parent spec
+    opts = { use_diagnostic_signs = true },
+  },
+}
