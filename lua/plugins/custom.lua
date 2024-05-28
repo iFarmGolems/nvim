556bcb22efcda892d9a129336f52e5b78b45a302
Patrik Jajcay <patrik.jajcay@microstep-mis.com>
2024-05-28 08:58:32 +0200

    Progress

--- lua/plugins/custom.lua
+++ lua/plugins/custom.lua
@@ -13,7 +13,31 @@ return {
   {
     "pmizio/typescript-tools.nvim",
     dependencies = { "nvim-lua/plenary.nvim", "neovim/nvim-lspconfig" },
-    opts = {},
+    config = function()
+      -- local lspconfig = require('lspconfig')
+
+      -- lspconfig.typescript.setup({
+      --   on_attach = function(client, bufnr)
+      --     client.resolved_capabilities.document_formatting = false
+      --     client.resolved_capabilities.document_range_formatting = false
+      --     require('lsp-status').on_attach(client, bufnr)
+      --   end,
+      -- })
+
+      require("typescript-tools").setup({
+        handlers = {
+          ["textDocument/publishDiagnostics"] = function(err, res, ctx, config)
+            local isJS = res.uri:sub(-3) == ".js"
+
+            if isJS then
+              res.diagnostics = {}
+            end
+
+            vim.lsp.diagnostic.on_publish_diagnostics(err, res, ctx, config)
+          end,
+        },
+      })
+    end,
   },
   {
     "ahmedkhalf/project.nvim",
