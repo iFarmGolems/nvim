044c75c1c6fb98ef2f1055dd1c325be52bec726d
Patrik Jajcay <patrik.jajcay@microstep-mis.com>
2024-05-28 09:02:25 +0200

    Remove comment

--- lua/plugins/custom.lua
+++ lua/plugins/custom.lua
@@ -9,20 +9,11 @@ return {
       colorscheme = "gruvbox",
     },
   },
-  -- TS
   {
     "pmizio/typescript-tools.nvim",
     dependencies = { "nvim-lua/plenary.nvim", "neovim/nvim-lspconfig" },
     config = function()
       -- local lspconfig = require('lspconfig')
-
-      -- lspconfig.typescript.setup({
-      --   on_attach = function(client, bufnr)
-      --     client.resolved_capabilities.document_formatting = false
-      --     client.resolved_capabilities.document_range_formatting = false
-      --     require('lsp-status').on_attach(client, bufnr)
-      --   end,
-      -- })
 
       require("typescript-tools").setup({
         handlers = {
