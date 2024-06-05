1708c013f78cdb9206cfdc93792b2d66d8624292
Patrik Jajcay <patrik.jajcay@microstep-mis.com>
2024-06-05 12:13:30 +0200

    No ruler for now

--- lua/config/autocmds.lua
+++ lua/config/autocmds.lua
@@ -2,6 +2,6 @@
 -- Default autocmds that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/autocmds.lua
 -- Add any additional autocmds here
 
-vim.cmd([[
-  autocmd FileType javascript setlocal colorcolumn=80
-]])
+-- vim.cmd([[
+--   autocmd FileType javascript setlocal colorcolumn=80
+-- ]])
