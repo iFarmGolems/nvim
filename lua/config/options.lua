0b409dc8e8814ee2265503e6ab728ff5e8b4876a
Patrik Jajcay <patrik.jajcay@microstep-mis.com>
2024-06-09 00:41:59 +0200

    Update options

--- lua/config/options.lua
+++ lua/config/options.lua
@@ -7,16 +7,15 @@ if vim.g.neovide then
   vim.cmd("cd /home/patrik/develop/repos")
 
   -- font https://neovide.dev/configuration.html?highlight=font#display
-  vim.o.guifont = "Comic Code Ligatures:h12"
-  -- vim.g.neovide_transparency = 0.97
+  vim.o.guifont = "Comic Code Ligatures,ComicShannsMono Nerd Font Mono:h12"
+  vim.g.neovide_transparency = 0.97
 end
 
-vim.opt.termguicolors = true
 vim.g.gruvbox_baby_comment_style = "NONE"
--- vim.g.gruvbox_baby_transparent_mode = true
+vim.opt.termguicolors = true
 
 -- vim.opt.autoindent = true
 -- vim.opt.smartindent = true
-vim.opt.tabstop = 2
-vim.opt.shiftwidth = 2
-vim.opt.expandtab = false
+-- vim.opt.tabstop = 2
+-- vim.opt.shiftwidth = 2
+-- vim.opt.expandtab = false
