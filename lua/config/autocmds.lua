e4a37ab7536e58708184154981cc7f6822c1292c
Patrik Jajcay <patrik.jajcay@microstep-mis.com>
2024-06-10 22:57:33 +0200

    Ruler for JS and Markdown

--- lua/config/autocmds.lua
+++ lua/config/autocmds.lua
@@ -2,6 +2,10 @@
 -- Default autocmds that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/autocmds.lua
 -- Add any additional autocmds here
 
--- vim.cmd([[
---   autocmd FileType javascript setlocal colorcolumn=80
--- ]])
+vim.cmd([[
+  autocmd FileType javascript setlocal colorcolumn=80
+]])
+
+vim.cmd([[
+  autocmd FileType markdown setlocal colorcolumn=80
+]])
