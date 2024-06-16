d961728a5d362438ec119dde1f8181b0d9424c60
Patrik Jajcay <patrik.jajcay@microstep-mis.com>
2024-06-16 17:50:18 +0200

    Make x only delete the text - not save it

--- lua/config/keymaps.lua
+++ lua/config/keymaps.lua
@@ -1,11 +1,5 @@
 -- Keymaps are automatically loaded on the VeryLazy event
 -- Default keymaps that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/keymaps.lua
--- Add any additional keymaps here
-if vim.g.neovide then
-  -- vim.keymap.set("n", "<D-s>", ":w<CR>") -- Save
-  -- vim.keymap.set("v", "<D-c>", '"+y') -- Copy
-  -- vim.keymap.set("n", "<D-v>", '"+P') -- Paste normal mode
-  -- vim.keymap.set("v", "<D-v>", '"+P') -- Paste visual mode
-  -- vim.keymap.set("c", "<D-v>", "<C-R>+") -- Paste command mode
-  -- vim.keymap.set("i", "<D-v>", '<ESC>l"+Pli') -- Paste insert mode
-end
+
+-- deleting with "x" will not save the deleted text to the register
+vim.keymap.set("n", "x", '"_x')
