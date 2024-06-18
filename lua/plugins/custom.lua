3495b8cd9717c7fbf6d7fe5439e3d35b825b54ad
Patrik Jajcay <patrik.jajcay@microstep-mis.com>
2024-06-18 08:46:21 +0200

    Silent = true

--- lua/plugins/custom.lua
+++ lua/plugins/custom.lua
@@ -82,7 +82,7 @@ return {
     "ahmedkhalf/project.nvim",
     opts = function(_, opts)
       opts.manual_mode = false
-      opts.silent_chdir = false
+      opts.silent_chdir = true
     end,
   },
   {
