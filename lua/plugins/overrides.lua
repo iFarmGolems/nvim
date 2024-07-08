2d9e256bc7b1b4201dc2ea8045cde1403028205b
iFarmGolems <patrikjajcay@gmail.com>
2024-07-09 00:37:46 +0200

    Show variables and constants in symbol lookup

--- lua/plugins/overrides.lua
+++ lua/plugins/overrides.lua
@@ -25,4 +25,29 @@ return {
       opts.silent_chdir = true
     end,
   },
+
+  {
+    "LazyVim/LazyVim",
+    opts = {
+      kind_filter = {
+        default = {
+          "Class",
+          "Constant", -- add this
+          "Constructor",
+          "Enum",
+          "Field",
+          "Function",
+          "Interface",
+          "Method",
+          "Module",
+          "Namespace",
+          "Package",
+          "Property",
+          "Struct",
+          "Trait",
+          "Variable", -- add this
+        },
+      },
+    },
+  },
 }
