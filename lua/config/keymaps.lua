4104c2653f3d36864716ac80e913cdc46d470548
Patrik Jajcay <patrik.jajcay@microstep-mis.com>
2024-07-22 13:30:16 +0200

    Right keymap

--- lua/config/keymaps.lua
+++ lua/config/keymaps.lua
@@ -1,36 +1,32 @@
 -- deleting with "x" will not save the deleted text to the register
 vim.keymap.set("n", "x", '"_x')
 
--- spectre
+-- grug-far overrides
 vim.keymap.del("n", "<leader>sr")
+vim.keymap.del("v", "<leader>sr")
 
-vim.keymap.set(
-  "n",
-  "<leader>sri",
-  '<cmd>lua require("grug-far").grug_far({ prefills = { search = vim.fn.expand("<cword>"), flags = vim.fn.expand("%") } })<CR>',
-  {
-    desc = "Replace current word in current file",
-  }
-)
+-- normal mode
+vim.keymap.set("n", "<leader>sri", function()
+  require("grug-far").grug_far({ prefills = { search = vim.fn.expand("<cword>"), flags = vim.fn.expand("%") } })
+end, {
+  desc = "Replace current word in current file",
+})
 
-vim.keymap.set(
-  "n",
-  "<leader>sra",
-  '<cmd>lua require("grug-far").grug_far({ prefills = { search = vim.fn.expand("<cword>") } })<CR>',
-  {
-    desc = "Replace current word in all files",
-  }
-)
+vim.keymap.set("n", "<leader>sra", function()
+  require("grug-far").grug_far({ prefills = { search = vim.fn.expand("<cword>") } })
+end, {
+  desc = "Replace current word in all files",
+})
 
-vim.keymap.set(
-  "v",
-  "<leader>srvi",
-  '<cmd><C-u>lua require("grug-far").with_visual_selection({ flags = vim.fn.expand("%") })<CR>',
-  {
-    desc = "Replace visual selection in current file",
-  }
-)
+-- visual mode
+vim.keymap.set("v", "<leader>sri", function()
+  require("grug-far").with_visual_selection({ prefills = { flags = vim.fn.expand("%") } })
+end, {
+  desc = "Replace visual selection in current file",
+})
 
-vim.keymap.set("v", "<leader>srva", '<cmd>lua require("grug-far").with_visual_selection()<CR>', {
+vim.keymap.set("v", "<leader>sra", function()
+  require("grug-far").with_visual_selection()
+end, {
   desc = "Replace visual selection in all files",
 })
