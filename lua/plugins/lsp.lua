979193720a5fae9042652cda42fa641e02eb1908
Patrik Jajcay <patrik.jajcay@microstep-mis.com>
2024-08-01 12:46:09 +0200

    Biome support

--- lua/plugins/lsp.lua
+++ lua/plugins/lsp.lua
@@ -1,7 +1,39 @@
+local function biome_lsp_or_prettier(bufnr)
+  local has_biome_lsp = vim.lsp.get_active_clients({
+    bufnr = bufnr,
+    name = "biome",
+  })[1]
+
+  if has_biome_lsp then
+    return {}
+  end
+
+  local has_prettier = vim.fs.find({
+    ".prettierrc",
+    ".prettierrc.json",
+    ".prettierrc.yml",
+    ".prettierrc.yaml",
+    ".prettierrc.json5",
+    ".prettierrc.js",
+    ".prettierrc.cjs",
+    ".prettierrc.toml",
+    "prettier.config.js",
+    "prettier.config.cjs",
+  }, { upward = true })[1]
+
+  if has_prettier then
+    return { "prettier" }
+  end
+
+  return { "biome" }
+end
+
 return {
+
   { "hrsh7th/nvim-cmp", opts = {
     completion = { autocomplete = false },
   } },
+
   {
     "neovim/nvim-lspconfig",
     opts = {
@@ -19,6 +51,37 @@ return {
             },
           },
         },
+      },
+    },
+  },
+
+  {
+    "stevearc/conform.nvim",
+    optional = true,
+    opts = {
+      -- for language support
+      -- @see https://biomejs.dev/internals/language-support/
+      formatters_by_ft = {
+        ["javascript"] = { "biome" },
+        ["javascriptreact"] = { "biome" },
+        ["typescript"] = { "biome" },
+        ["typescriptreact"] = { "biome" },
+        ["json"] = { "biome" },
+        ["jsonc"] = { "biome" },
+        ["vue"] = { "biome" },
+        ["css"] = { "biome" },
+        ["scss"] = { "biome" },
+        ["less"] = { "biome" },
+        ["html"] = { "biome" },
+      },
+    },
+  },
+
+  {
+    "williamboman/mason.nvim",
+    opts = {
+      ensure_installed = {
+        "biome",
       },
     },
   },
