09b91d5dfac8b2483490cc7f914f9e2093b0b67b
Patrik Jajcay <patrik.jajcay@microstep-mis.com>
2025-01-15 14:36:59 +0100

    Command for copying current buffer path (from gitroot if available)

--- lua/config/options.lua
+++ lua/config/options.lua
@@ -32,3 +32,16 @@ vim.g.grubbox_baby_background_color = "dark"
 vim.opt.termguicolors = true
 vim.opt.list = false
 vim.opt.colorcolumn = "81"
+
+vim.api.nvim_create_user_command("CopyPath", function()
+  local git_root = vim.fn.systemlist("git rev-parse --show-toplevel")[1]
+  local file_path = vim.fn.expand("%:p")
+  local path_to_copy = file_path
+
+  if vim.v.shell_error == 0 then
+    path_to_copy = file_path:sub(#git_root + 2)
+  end
+
+  vim.fn.setreg("+", path_to_copy)
+  print("Copied: " .. path_to_copy)
+end, {})
