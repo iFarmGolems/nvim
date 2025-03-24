2d064afe86bf8213c4e28774d7a57168322dfb86
Patrik Jajcay <patrik.jajcay@microstep-mis.com>
2025-03-24 23:48:13 +0100

    Cleanup

--- lua/plugins/lsp.lua
+++ lua/plugins/lsp.lua
@@ -6,59 +6,6 @@ return {
     opts = {
       inlay_hints = { enabled = false },
       servers = {
-        -- eslint = {
-        --   root_dir = function(...)
-        --     return nvim_lsp.util.root_pattern(
-        --       "eslint.config.js",
-        --       "eslint.config.mjs",
-        --       "eslint.config.cjs",
-        --       "eslint.config.ts",
-        --       "eslint.config.mts",
-        --       "eslint.config.cts",
-        --       ".eslintrc.js",
-        --       ".eslintrc.yaml",
-        --       ".eslintrc.yml",
-        --       ".eslintrc.json"
-        --     )(...)
-        --   end,
-        -- },
-        -- eslint_d = {
-        --   root_dir = function(...)
-        --     return nvim_lsp.util.root_pattern(
-        --       "eslint.config.js",
-        --       "eslint.config.mjs",
-        --       "eslint.config.cjs",
-        --       "eslint.config.ts",
-        --       "eslint.config.mts",
-        --       "eslint.config.cts",
-        --       ".eslintrc.js",
-        --       ".eslintrc.yaml",
-        --       ".eslintrc.yml",
-        --       ".eslintrc.json"
-        --     )(...)
-        --   end,
-        -- },
-        denols = {
-          filetypes = { "typescript", "typescriptreact" },
-          root_dir = function(...)
-            return nvim_lsp.util.root_pattern("deno.jsonc", "deno.json")(...)
-          end,
-          settings = {
-            deno = {
-              offset_encoding = "utf-8",
-              enable = true,
-              lint = true,
-              unstable = true,
-              suggest = {
-                imports = {
-                  hosts = {
-                    ["https://deno.land"] = true,
-                  },
-                },
-              },
-            },
-          },
-        },
         vtsls = {
           single_file_support = false,
           root_dir = nvim_lsp.util.root_pattern("package.json"),
