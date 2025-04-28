468f2f94322841f9eff540d8a4a0a36de4f394c0
Patrik Jajcay <patrik.jajcay@microstep-mis.com>
2025-04-28 13:32:50 +0200

    Use typescript-tools now

--- lua/plugins/extra.lua
+++ lua/plugins/extra.lua
@@ -12,6 +12,16 @@ return {
   },
 
   {
+    "pmizio/typescript-tools.nvim",
+    dependencies = { "nvim-lua/plenary.nvim", "neovim/nvim-lspconfig" },
+    opts = {
+      -- settings = {
+      --   separate_diagnostic_server = false,
+      -- },
+    },
+  },
+
+  {
     "iFarmGolems/deploy.nvim",
     dependencies = { "nvim-lua/plenary.nvim" },
     config = function()
--- lua/plugins/lsp.lua
+++ lua/plugins/lsp.lua
@@ -6,21 +6,21 @@ return {
     opts = {
       inlay_hints = { enabled = false },
       servers = {
-        vtsls = {
-          single_file_support = false,
-          root_dir = nvim_lsp.util.root_pattern("package.json"),
-          settings = {
-            javascript = {
-              suggest = {
-                names = false,
-                paths = false,
-                autoImports = false,
-                includeCompletionsForImportStatements = false,
-                completeFunctionCalls = false,
-              },
-            },
-          },
-        },
+        -- vtsls = {
+        --   single_file_support = false,
+        --   root_dir = nvim_lsp.util.root_pattern("package.json"),
+        --   settings = {
+        --     javascript = {
+        --       suggest = {
+        --         names = false,
+        --         paths = false,
+        --         autoImports = false,
+        --         includeCompletionsForImportStatements = false,
+        --         completeFunctionCalls = false,
+        --       },
+        --     },
+        --   },
+        -- },
       },
     },
   },
